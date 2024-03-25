f91bcdeecec4dbc0fa582038921c93f155a3fa01
Sushree Ranjan Moharana <sushree.moharana@yellowmessenger.com>
2024-03-25 17:14:08 +0530

    fix(exports): avoid barrel files

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-export { default as function1 } from "./components/component1";
-export { default as function2 } from "./components/component2";
+// export { default as function1 } from "./components/component1";
+// export { default as function2 } from "./components/component2";
+
+const function1 = () => {
+  console.log("Inside function1");
+};
+
+const function2 = () => {
+  console.log("Inside function2");
+};
+
+export { function1, function2 };
